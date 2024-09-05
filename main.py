468881624e261a4fa1d20400841c7620435c424b
Rabenwall <matthias.strassegger@gmail.com>
2024-09-05 12:04:17 +0200

    refactor

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while 1 == 1:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill("black")
         pygame.display.flip()
 
 if __name__ == "__main__":
